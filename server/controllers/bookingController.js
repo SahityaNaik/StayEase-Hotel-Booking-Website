55fd9a7d3a4f3b6f2e9d8cc41c5b015d688ae5d9
SahityaNaik <sahityanaik03@gmail.com>
2025-08-15 12:53:09 +0530

    currency update

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -145,7 +145,7 @@ export const stripePayment = async (req, res)=>{
         const line_items = [
             {
                 price_data:{
-                    currency: "usd",
+                    currency: "inr",
                     product_data:{
                         name: roomData.hotel.name,
                     },
